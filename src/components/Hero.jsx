91685d216df5aa8c9a2841fa35febb82c28caad6
Faixan6190 <faixan6190@gmail.com>
2024-05-25 18:30:44 +0500

    checking

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -33,7 +33,6 @@ const Hero = () => (
         We examine annual percentage rates, annual fees.
       </p>
     </div>
-
     <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
       <img src={robot} alt="bliing" className="w-[100%] h-[100%] relative z-[5]" />
       <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
