3c3624875da6a571f0c7ffcd4c8bba7ecb5c5118
Faixan6190 <faixan6190@gmail.com>
2024-05-26 22:29:22 +0500

    change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,24 +6,13 @@ const Footer = () => (
   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
     <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
       <div className="flex-1 flex flex-col justify-start mr-10">
-        <img
-          src={logo}
-          alt="hoobank"
-          className="w-[266px] h-[72px] object-contain"
-        />
-        <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
-          A new way to make the payments easy, reliable and secure.
-        </p>
+        <img src={logo} alt="hoobank" className="w-[266px] h-[72px] object-contain" />
+        <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>A new way to make the payments easy, reliable and secure.</p>
       </div>
       <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
         {footerLinks.map((footerLink) => (
-          <div
-            key={footerLink.key + footerLink.title}
-            className="flex flex-col ss:my-0 my-4 min-w-[150px]"
-          >
-            <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
-              {footerLink.title}
-            </h4>
+          <div key={footerLink.key + footerLink.title} className="flex flex-col ss:my-0 my-4 min-w-[150px]">
+            <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">{footerLink.title}</h4>
             <ul className="list-none mt-4">
               {footerLink.links.map((Link, index) => (
                 <li
@@ -42,7 +31,7 @@ const Footer = () => (
     </div>
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        2021 Hoobank. All Rights Reserved.
+        2021 Hoobank. All Rights Reserved...
       </p>
       <div className="flex flex-row md:mt-0 mt-6 ">
         {socialMedia.map((social, index) => (
@@ -50,9 +39,7 @@ const Footer = () => (
             key={social.id}
             src={social.icon}
             alt={social.id}
-            className={`w-[21px] h-[21px] object-contain cursor-pointer ${
-              index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
-            }`}
+            className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? "mr-6" : "mr-0"}`}
           />
         ))}
       </div>
